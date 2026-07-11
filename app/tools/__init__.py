b051b19d347dca6fe8df9698492f99e789769ccb
Sayed <102337556+Sayedc@users.noreply.github.com>
2026-07-12 01:37:27 +0300

    Add ForexTool to load_tools function

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -1,9 +1,11 @@
 from app.tools.calculator import CalculatorTool
 from app.tools.crypto import CryptoTool
+from app.tools.forex import ForexTool
 
 
 def load_tools():
     return [
         CalculatorTool(),
         CryptoTool(),
+        ForexTool(),
     ]
