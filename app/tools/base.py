43353fb680956aa707c8804c87865e0b2599ce79
Sayed <102337556+Sayedc@users.noreply.github.com>
2026-07-12 01:28:44 +0300

    Update base.py

--- app/tools/base.py
+++ app/tools/base.py
@@ -2,9 +2,14 @@ from abc import ABC, abstractmethod
 
 
 class Tool(ABC):
+
     name: str
     description: str
 
     @abstractmethod
     async def run(self, query: str):
+        pass
+
+    @abstractmethod
+    def can_handle(self, query: str) -> bool:
         pass
