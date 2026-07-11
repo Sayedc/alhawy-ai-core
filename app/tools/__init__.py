4e29778f0fea0a3725fc9e3baa6b78e4079da7b4
Sayed <102337556+Sayedc@users.noreply.github.com>
2026-07-12 01:34:24 +0300

    Import tools and define load_tools function

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -1,1 +1,9 @@
+from app.tools.calculator import CalculatorTool
+from app.tools.crypto import CryptoTool
 
+
+def load_tools():
+    return [
+        CalculatorTool(),
+        CryptoTool(),
+    ]
