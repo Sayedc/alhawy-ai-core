1d4dfb05c31d603a61fe5b1d9fe7d3bc6563a70e
Sayed <102337556+Sayedc@users.noreply.github.com>
2026-07-14 07:17:35 +0300

    Update __init__.py

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -1,11 +1,21 @@
 from app.tools.calculator import CalculatorTool
 from app.tools.crypto import CryptoTool
 from app.tools.forex import ForexTool
+from app.tools.gold import GoldTool
+from app.tools.news import NewsTool
+from app.tools.stocks import StocksTool
+
+from app.tools.registry import registry
 
 
 def load_tools():
-    return [
-        CalculatorTool(),
-        CryptoTool(),
-        ForexTool(),
-    ]
+    registry.clear()
+
+    registry.register(CalculatorTool())
+    registry.register(CryptoTool())
+    registry.register(ForexTool())
+    registry.register(GoldTool())
+    registry.register(NewsTool())
+    registry.register(StocksTool())
+
+    return registry
